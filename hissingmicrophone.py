9b7de640ed7ee614a8d8224e3925f180962ffaee
zrthstr <zrth1k@gmail.com>
2018-12-19 09:51:16 +0100

    ..

---
+++ hissingmicrophone.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from collections import Counter
+
+if __name__ == "__main__":
+
+    i = input()
+    if "ss" in i:
+        print("hiss")
+    else:
+        print("no hiss")
